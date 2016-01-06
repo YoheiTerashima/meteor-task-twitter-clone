7f1e708449bab68ae2c23328858dc30c146f84de
寺島 洋平 <yamanogomora@gmail.com>
2016-01-06 21:14:50 +0900

    step03: Collection

--- task-twitter-clone.js
+++ task-twitter-clone.js
@@ -1,15 +1,23 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({});
+    }
+  });
+
   Template.hello.helpers({
-    counter: function () {
+    counter: function() {
       return Session.get('counter');
     }
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click button': function() {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
@@ -17,7 +25,7 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
